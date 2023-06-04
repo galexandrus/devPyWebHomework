beef0873534ced81b07514893c11428a3ae199c4
galexandrus <gavrilchuk_a@mail.ru>
2023-06-04 08:16:02 +0300

    add login app

--- other/urls.py
+++ other/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from views import CurrentDateView
+from .views import CurrentDateView, Hello, RandomNumber, IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('datetime/', CurrentDateView.as_view()),
+    path('hello/', Hello.as_view()),
+    path('random_number/', RandomNumber.as_view()),
 ]
--- other/views.py
+++ other/views.py
@@ -1,11 +1,30 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from datetime import datetime
 
 from django.views import View
 from django.http import HttpResponse
 
+import random
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'other/index.html')
+
 
 class CurrentDateView(View):
     def get(self, request):
         html = f"{datetime.now()}"
+        return HttpResponse(html)
+
+
+class Hello(View):
+    def get(self, request):
+        html = "<h1>Say hello to my little friend!</h1>"
+        return HttpResponse(html)
+
+
+class RandomNumber(View):
+    def get(self, request):
+        html = f"{random.randrange(0, 101)}"
         return HttpResponse(html)
