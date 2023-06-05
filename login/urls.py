1e9c1658d32bb0826dab714db488e0e9a6757667
galexandrus <gavrilchuk_a@mail.ru>
2023-06-05 06:40:08 +0300

    add post request handler to login app

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import LoginView
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', LoginView.as_view()),
 ]
--- login/views.py
+++ login/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 from django.views import View
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 
-class IndexView(View):
+class LoginView(View):
     def get(self, request):
         return render(request, 'login/index.html')
+
+    def post(self, request):
+        return JsonResponse(request.POST, json_dumps_params={"indent": 4})
