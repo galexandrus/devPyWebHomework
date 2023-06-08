a0176f7f7bb13ca22593ae61a581838c0908f435
galexandrus <gavrilchuk_a@mail.ru>
2023-06-08 07:13:04 +0300

    add block {% for %} in shop.html & update store/views.py

--- store/views.py
+++ store/views.py
@@ -15,4 +15,92 @@ class ProductSingleView(View):
 
 class ShopView(View):
     def get(self, request):
-        return render(request, 'store/shop.html')
+        context = {
+            'data': [
+                {
+                    'name': 'Bell Pepper',
+                    'discount': 25,
+                    'price_before': 120.00,
+                    'price_after': 95.00,
+                    'url': 'store/images/product-1.jpg',
+                },
+                {
+                    'name': 'Strawberry',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-2.jpg',
+                },
+                {
+                    'name': 'Green Beans',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-3.jpg',
+                },
+                {
+                    'name': 'Purple Cabbage',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-4.jpg',
+                },
+                {
+                    'name': 'Tomato',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-5.jpg',
+                },
+                {
+                    'name': 'Brocolli',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-6.jpg',
+                },
+                {
+                    'name': 'Carrots',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-7.jpg',
+                },
+                {
+                    'name': 'Fruit Juice',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-8.jpg',
+                },
+                {
+                    'name': 'Onion',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-9.jpg',
+                },
+                {
+                    'name': 'Apple',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-10.jpg',
+                },
+                {
+                    'name': 'Garlic',
+                    'discount': 0,
+                    'price_before': 120.00,
+                    'price_after': 120.00,
+                    'url': 'store/images/product-11.jpg',
+                },
+                {
+                    'name': 'Chilli',
+                    'discount': 20,
+                    'price_before': 120.00,
+                    'price_after': 100.00,
+                    'url': 'store/images/product-12.jpg',
+                },
+            ]
+        }
+        return render(request, 'store/shop.html', context)
