db07495175cb18a81ccd6b8a1c76daecb427f536
galexandrus <gavrilchuk_a@mail.ru>
2023-06-14 08:07:21 +0300

    update store/views.py to get products from database

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from django.views import View
 
+from django.db.models import OuterRef, Subquery, ExpressionWrapper, DecimalField, Case, When, F
+from django.utils import timezone
+from .models import Product, Discount
+
 
 class CartView(View):
     def get(self, request):
@@ -101,104 +105,135 @@ class ProductSingleView(View):
 
 class ShopView(View):
     def get(self, request):
-        context = {
-            'data': [
-                {
-                    'name': 'Bell Pepper',
-                    'discount': 25,
-                    'price_before': 120.00,
-                    'price_after': 90.00,
-                    'id': 1,
-                    'url': 'store/images/product-1.jpg',
-                },
-                {
-                    'name': 'Strawberry',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 2,
-                    'url': 'store/images/product-2.jpg',
-                },
-                {
-                    'name': 'Green Beans',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 3,
-                    'url': 'store/images/product-3.jpg',
-                },
-                {
-                    'name': 'Purple Cabbage',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 4,
-                    'url': 'store/images/product-4.jpg',
-                },
-                {
-                    'name': 'Tomato',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 5,
-                    'url': 'store/images/product-5.jpg',
-                },
-                {
-                    'name': 'Brocolli',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 6,
-                    'url': 'store/images/product-6.jpg',
-                },
-                {
-                    'name': 'Carrot',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 7,
-                    'url': 'store/images/product-7.jpg',
-                },
-                {
-                    'name': 'Fruit Juice',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 8,
-                    'url': 'store/images/product-8.jpg',
-                },
-                {
-                    'name': 'Onion',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 9,
-                    'url': 'store/images/product-9.jpg',
-                },
-                {
-                    'name': 'Apple',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 10,
-                    'url': 'store/images/product-10.jpg',
-                },
-                {
-                    'name': 'Garlic',
-                    'discount': None,
-                    'price_before': 120.00,
-                    'price_after': 120.00,
-                    'id': 11,
-                    'url': 'store/images/product-11.jpg',
-                },
-                {
-                    'name': 'Chilli',
-                    'discount': 20,
-                    'price_before': 120.00,
-                    'price_after': 96.00,
-                    'id': 12,
-                    'url': 'store/images/product-12.jpg',
-                },
-            ]
-        }
-        return render(request, 'store/shop.html', context)
+        # Request for all actual discounts
+        discount_value = Case(
+            When(
+                discount_value_gte=0,
+                discount_date_begin_lte=timezone.now(),
+                discount_date_end_gte=timezone.now(),
+                then=F('discount_value')
+            ),
+            default=0,
+            output_field=DecimalField(max_digits=10, decimal_places=2)
+        )
+
+        # Request for calculate price with discount
+        price_with_discount = ExpressionWrapper(
+            F('price') * (100.0 - F('discount_value')) / 100.0,
+            output_field=DecimalField(max_digits=10, decimal_places=2)
+        )
+
+        products = Product.objects.annotate(
+            discount_value=discount_value,
+            # Alternative way with subquery:
+            # discount_value=Subquery(
+            #     Discount.objects.filter(product_id=OuterRef('id').values('value'))
+            # ),
+            price_before=F('price'),
+            price_after=price_with_discount,
+        ).values('id', 'name', 'image', 'price_before', 'price_after', 'discount_value')
+
+        return render(request, 'store/shop.html', {"data": products})
+
+    # def get(self, request):
+        # context = {
+        #     'data': [
+        #         {
+        #             'name': 'Bell Pepper',
+        #             'discount': 25,
+        #             'price_before': 120.00,
+        #             'price_after': 90.00,
+        #             'id': 1,
+        #             'url': 'store/images/product-1.jpg',
+        #         },
+        #         {
+        #             'name': 'Strawberry',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 2,
+        #             'url': 'store/images/product-2.jpg',
+        #         },
+        #         {
+        #             'name': 'Green Beans',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 3,
+        #             'url': 'store/images/product-3.jpg',
+        #         },
+        #         {
+        #             'name': 'Purple Cabbage',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 4,
+        #             'url': 'store/images/product-4.jpg',
+        #         },
+        #         {
+        #             'name': 'Tomato',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 5,
+        #             'url': 'store/images/product-5.jpg',
+        #         },
+        #         {
+        #             'name': 'Brocolli',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 6,
+        #             'url': 'store/images/product-6.jpg',
+        #         },
+        #         {
+        #             'name': 'Carrot',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 7,
+        #             'url': 'store/images/product-7.jpg',
+        #         },
+        #         {
+        #             'name': 'Fruit Juice',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 8,
+        #             'url': 'store/images/product-8.jpg',
+        #         },
+        #         {
+        #             'name': 'Onion',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 9,
+        #             'url': 'store/images/product-9.jpg',
+        #         },
+        #         {
+        #             'name': 'Apple',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 10,
+        #             'url': 'store/images/product-10.jpg',
+        #         },
+        #         {
+        #             'name': 'Garlic',
+        #             'discount': None,
+        #             'price_before': 120.00,
+        #             'price_after': 120.00,
+        #             'id': 11,
+        #             'url': 'store/images/product-11.jpg',
+        #         },
+        #         {
+        #             'name': 'Chilli',
+        #             'discount': 20,
+        #             'price_before': 120.00,
+        #             'price_after': 96.00,
+        #             'id': 12,
+        #             'url': 'store/images/product-12.jpg',
+        #         },
+        #     ]
+        # }
+        # return render(request, 'store/shop.html', context)
