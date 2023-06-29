3869f9325af331cffefa5c6057ce1bac89450c80
galexandrus <gavrilchuk_a@mail.ru>
2023-06-29 08:24:05 +0300

    fix bug with product-single show

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,5 @@ app_name = 'store'
 urlpatterns = [
     path('', ShopView.as_view(), name='shop'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('product/<int:d>/', ProductSingleView.as_view(), name='product'),
+    path('product/<int:id>/', ProductSingleView.as_view(), name='product'),
 ]
