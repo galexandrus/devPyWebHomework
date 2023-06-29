934442d1dabac58d1efc0e3c654516f352c9578b
galexandrus <gavrilchuk_a@mail.ru>
2023-06-29 16:11:59 +0300

    prepare to deploy on pythonanywhere

--- test_store/settings.py
+++ test_store/settings.py
@@ -145,7 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+if 'localhost' in ALLOWED_HOSTS:
+    STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
